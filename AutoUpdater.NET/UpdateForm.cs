1883c8c40a4c92bd7cff5b2fa96e1bc45cba95ba
leegk <leegk@DESKTOP-MEI4SUM>
2016-09-04 14:57:03 +0800

    Clone from codeplex

---
+++ AutoUpdater.NET/UpdateForm.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace AutoUpdaterDotNET
+{
+    internal partial class UpdateForm : Form
+    {
+        private System.Timers.Timer _timer;
+        
+        private bool HideReleaseNotes { get; set; }
+
+        public UpdateForm(bool remindLater = false)
+        {
+            if (!remindLater)
+            {
+                InitializeComponent();
+                var resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
+                Text = string.Format(resources.GetString("$this.Text", CultureInfo.CurrentCulture), AutoUpdater.AppTitle, AutoUpdater.CurrentVersion);
+                labelUpdate.Text = string.Format(resources.GetString("labelUpdate.Text", CultureInfo.CurrentCulture), AutoUpdater.AppTitle);
+                labelDescription.Text =
+                    string.Format(resources.GetString("labelDescription.Text", CultureInfo.CurrentCulture),
+                        AutoUpdater.AppTitle, AutoUpdater.CurrentVersion, AutoUpdater.InstalledVersion);
+                if (string.IsNullOrEmpty(AutoUpdater.ChangeLogURL))
+                {
+                    HideReleaseNotes = true;
+                    var reduceHeight = labelReleaseNotes.Height + webBrowser.Height;
+                    labelReleaseNotes.Hide();
+                    webBrowser.Hide();
+
+                    Height -= reduceHeight;
+
+                    buttonSkip.Location = new Point(buttonSkip.Location.X, buttonSkip.Location.Y - reduceHeight);
+                    buttonRemindLater.Location = new Point(buttonRemindLater.Location.X, buttonRemindLater.Location.Y - reduceHeight);
+                    buttonUpdate.Location = new Point(buttonUpdate.Location.X, buttonUpdate.Location.Y - reduceHeight);
+                }
+            }
+        }
+
+        public override sealed string Text
+        {
+            get { return base.Text; }
+            set { base.Text = value; }
+        }
+
+        private void UpdateFormLoad(object sender, EventArgs e)
+        {
+            if (HideReleaseNotes == false)
+            {
+                webBrowser.Navigate(AutoUpdater.ChangeLogURL);
+            }
+        }
+
+        private void ButtonUpdateClick(object sender, EventArgs e)
+        {
+            if (AutoUpdater.OpenDownloadPage)
+            {
+                var processStartInfo = new ProcessStartInfo(AutoUpdater.DownloadURL);
+
+                Process.Start(processStartInfo);
+            }
+            else
+            {
+                AutoUpdater.DownloadUpdate();
+            }
+        }
+
+        private void ButtonSkipClick(object sender, EventArgs e)
+        {
+            RegistryKey updateKey = Registry.CurrentUser.CreateSubKey(AutoUpdater.RegistryLocation);
+            if (updateKey != null)
+            {
+                updateKey.SetValue("version", AutoUpdater.CurrentVersion.ToString());
+                updateKey.SetValue("skip", 1);
+                updateKey.Close();
+            }
+        }
+
+        private void ButtonRemindLaterClick(object sender, EventArgs e)
+        {
+            if(AutoUpdater.LetUserSelectRemindLater)
+            {
+                var remindLaterForm = new RemindLaterForm();
+
+                var dialogResult = remindLaterForm.ShowDialog();
+
+                if(dialogResult.Equals(DialogResult.OK))
+                {
+                    AutoUpdater.RemindLaterTimeSpan = remindLaterForm.RemindLaterFormat;
+                    AutoUpdater.RemindLaterAt = remindLaterForm.RemindLaterAt;
+                }
+                else if(dialogResult.Equals(DialogResult.Abort))
+                {
+                    AutoUpdater.DownloadUpdate();
+                    return;
+                }
+                else
+                {
+                    DialogResult = DialogResult.None;
+                    return;
+                }
+            }
+
+            RegistryKey updateKey = Registry.CurrentUser.CreateSubKey(AutoUpdater.RegistryLocation);
+            if (updateKey != null)
+            {
+                updateKey.SetValue("version", AutoUpdater.CurrentVersion);
+                updateKey.SetValue("skip", 0);
+                DateTime remindLaterDateTime = DateTime.Now;
+                switch (AutoUpdater.RemindLaterTimeSpan)
+                {
+                    case RemindLaterFormat.Days:
+                        remindLaterDateTime = DateTime.Now + TimeSpan.FromDays(AutoUpdater.RemindLaterAt);
+                        break;
+                    case RemindLaterFormat.Hours:
+                        remindLaterDateTime = DateTime.Now + TimeSpan.FromHours(AutoUpdater.RemindLaterAt);
+                        break;
+                    case RemindLaterFormat.Minutes:
+                        remindLaterDateTime = DateTime.Now + TimeSpan.FromMinutes(AutoUpdater.RemindLaterAt);
+                        break;
+
+                }
+                updateKey.SetValue("remindlater", remindLaterDateTime.ToString(CultureInfo.CreateSpecificCulture("en-US")));
+                SetTimer(remindLaterDateTime);
+                updateKey.Close();
+            }
+        }
+
+        public void SetTimer(DateTime remindLater)
+        {
+            TimeSpan timeSpan = remindLater - DateTime.Now;
+            _timer = new System.Timers.Timer
+                {
+                    Interval = (int) timeSpan.TotalMilliseconds
+                };
+            _timer.Elapsed += TimerElapsed;
+            _timer.Start();
+        }
+
+        private void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            AutoUpdater.Start();
+        }
+    }
+}
